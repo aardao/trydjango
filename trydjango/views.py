999f0c69595f11d2c06b4215939f8f9d27692529
aardao <angel.ardao@gmail.com>
2021-12-26 16:30:35 +0100

    0.0.5 C16 una vista que lee una tabla

--- trydjango/views.py
+++ trydjango/views.py
@@ -4,9 +4,19 @@ To render html web pages
 
 from django.http  import HttpResponse
 
-HTML_STRING = """
-<h1>Hellow World</h1>
-"""
+
+from articles.models import Article
 
 def home_view(request):
+    name = "Justin"
+
+    article_obj = Article.objects.get(id=2)
+   
+
+    HTML_STRING = f"""
+    <h1>{article_obj.title}  ({article_obj.id})!</h1>
+    <p>{article_obj.content}!</p>
+    """
+
+   
     return HttpResponse(HTML_STRING)
