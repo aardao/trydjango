186fcb24361eedc85bc6580979175ea7cf44a263
aardao <angel.ardao@gmail.com>
2021-12-26 17:21:08 +0100

    0.0.6 C17 primeras templates

--- trydjango/views.py
+++ trydjango/views.py
@@ -3,20 +3,30 @@ To render html web pages
 """
 
 from django.http  import HttpResponse
-
-
+import random
 from articles.models import Article
+from django.template.loader import render_to_string
 
 def home_view(request):
     name = "Justin"
 
-    article_obj = Article.objects.get(id=2)
-   
+    ramdom_id = random.randint(1,3)
+    article_obj = Article.objects.get(id=ramdom_id)
+    
 
-    HTML_STRING = f"""
-    <h1>{article_obj.title}  ({article_obj.id})!</h1>
-    <p>{article_obj.content}!</p>
-    """
+    context = {
+        "object": article_obj,
+        "title": article_obj.title,
+        "id": article_obj.id,
+        "content": article_obj.content
+    }
+
+    HTML_STRING = render_to_string("home-view.html",context)
+
+    # HTML_STRING = """
+    # <h1>{title}  ({id})!</h1>
+    # <p>{content}!</p>
+    # """.format(**context)
 
    
     return HttpResponse(HTML_STRING)
