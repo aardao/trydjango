7b2f5827162a686100bdf58554300f4d0c94c4fd
aardao <angel.ardao@gmail.com>
2021-12-27 18:42:32 +0100

    0.0.10 C22 search form and request data

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 
 from .models import Article
 
+def article_search_view(request):
+    print('article_search_view')
+    print(request.GET)  
+    query_dict = request.GET  # this is a dictionary
+    
+    try:
+        query =int(query_dict.get("q"))    #<input type='text' name='q' />
+    except: 
+        query = None
+    print('query: ' , query)
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    context = {
+            "object" : article_obj
+    }
+    return render(request,"articles/search.html",context=context)
+
+
 # Create your views here.
 def article_detail_view(request,id=None):
     article_obj = None
