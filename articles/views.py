a43d414facb5737d6b1a0f34bd2b335f653761e2
aardao <angel.ardao@gmail.com>
2022-01-07 17:35:27 +0100

    0.0.29 C46 complex search using django Q

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from django.db.models import Q
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
 
@@ -8,44 +9,18 @@ from .forms import ArticleForm
 def article_search_view(request):
     print('article_search_view')
     #print(request.GET)  
-    query_dict = request.GET  # this is a dictionary
-    
-    try:
-        query =int(query_dict.get("q"))    #<input type='text' name='q' />
-    except: 
-        query = None
-    print('query: ' , query)
-    article_obj = None
+    query = request.GET.get('q')  
+    qs = Article.objects.all()
+
     if query is not None:
-        article_obj = Article.objects.get(id=query)
+        lookup = Q(title__icontains=query)  | Q(content__icontains=query)
+        qs = Article.objects.filter(lookup)
     context = {
-            "object" : article_obj
+            "object_list" : qs
     }
     return render(request,"articles/search.html",context=context)
 
-# @login_required
-# def article_create_view(request):
-#     #esto crea un formulario vacio siempre
-#     print('article_create_view parte1')
-#     form2 = ArticleForm()
-#     print('parte1')
-#     context={
-#         "form": form2
-#     }
-#     if request.method=="POST":
-#         #si el metodo es post, el formulario viene relleno
-#         print('article_create_view parte2')
-#         form=ArticleForm(request.POST)
-#         context['form']=form
-#         if form.is_valid():
-#             print('article_create_view parte3')
-#             title=form.cleaned_data.get("title")
-#             content=form.cleaned_data.get("content")
-#             print(title,content)
-#             article_object = Article.objects.create(title=title, content=content)
-#             context['object']=article_object
-#             context['created']=True
-#     return render(request,"articles/create.html",context)
+
 
 
 @login_required
