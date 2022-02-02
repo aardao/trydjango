b1236e55e8e00774f28e799addab196070f78a02
aardao <angel.ardao@gmail.com>
2022-02-02 23:28:04 +0100

    0.0.46 C66 - HTMX, JavaScript & Django Fixtures

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms.models import modelformset_factory #model for querysets
+from django.http import HttpResponse, Http404
+from django.urls import reverse
 
 from .models import Recipe, RecipeIngredient
 from .forms import RecipeForm, RecipeIngredientForm
@@ -15,9 +17,29 @@ def recipe_list_view(request):
 
 @login_required
 def recipe_detail_view(request, id=None):
-    obj = get_object_or_404(Recipe,id=id,user=request.user)
-    context = {"object": obj}
+    print('recipe_detail_view')
+    hx_url = reverse("recipes:hx-detail", kwargs={"id":id})
+    context = {
+        "hx_url": hx_url
+    }
     return render(request,"recipes\detail.html",context)
+
+@login_required
+def recipe_detail_hx_view(request, id=None):
+    print('recipe_detail_hx_view')
+    try:
+        obj = Recipe.objects.get(id=id,user=request.user)
+    except: 
+        obj = None
+    if obj is None:
+        
+        return HttpResponse("Not Found.")
+
+    context = {
+        "object": obj
+    }
+    return render(request,"recipes\partials\detail.html",context)
+
 
 @login_required
 def recipe_create_view(request):
